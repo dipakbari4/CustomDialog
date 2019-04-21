5a88a32b94dbe2bae6b9927c9ee24eff34b07ddb
Dipak Bari <dipakbari4@gmail.com>
2019-04-21 13:55:19 +0530

    Initial commit

---
+++ app/src/main/java/com/dipak/customdialog/MainActivity.java
@@ -0,0 +1,44 @@
+package com.dipak.customdialog;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openDialog(View view) {
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        View customDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null, false);
+
+        WebView webView = customDialog.findViewById(R.id.webview);
+
+        dialog.setContentView(customDialog);
+
+        webView.loadUrl("http://www.google.com");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        dialog.show();
+    }
+}
